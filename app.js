01a5e293e9428746913d6462d3937bae549efdf8
MohdNoman <mohammednoman757@gmail.com>
2024-05-19 20:05:38 +0530

    ejs-mate is install and views dir is set

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 const app  = express();
+import ejsMate from "ejs-mate";
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 // -------------------------
 // all route imports
@@ -26,6 +31,9 @@ async function main() {
 
 // all packages middlewares
 app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.engine('ejs',ejsMate);
+app.set("views",path.join(__dirname, "/views"));
 
 
 // all router middle wares
