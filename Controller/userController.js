fb2b9d083feadb583f86b884d1a49e32764ee268
MohdNoman <mohammednoman757@gmail.com>
2024-05-23 12:23:57 +0530

    user Routes Created

---
+++ Controller/userController.js
@@ -0,0 +1,28 @@
+
+
+let userProfile = async (req,res)=>{
+
+}
+
+let userUpdateProfile = async (req,res)=>{
+    
+}
+
+let userSignup = async (req,res)=>{
+    
+}
+
+let userSignCompleted = async (req,res)=>{
+    
+}
+
+let userLoginCompleted = async (req,res)=>{
+    
+}
+
+let userLogout = async (req,res)=>{
+    
+}
+
+
+export {userProfile,userUpdateProfile,userSignup,userSignCompleted,userLoginCompleted,userLogout};
--- Route/userRoute.js
+++ Route/userRoute.js
@@ -3,15 +3,28 @@ import e from "express";
 const router = e.Router({mergeParams: true});
 
 
+// Controller Import
+import {userProfile,userUpdateProfile,userSignup,userSignCompleted,userLoginCompleted,userLogout} from "../Controller/userController.js";
 
 
 
+// all user routes
+router.route('/')
+.get(userProfile);
 
+router.route('/:id')
+.put(userUpdateProfile);
 
+router.route('/signup')
+.get(userSignup)
+.post(userSignCompleted);
 
+router.route('/login')
+.get(userLogin)
+.post(userLoginCompleted);
 
-
-
+router.route('/logout')
+.get(userLogout);
 
 
 
